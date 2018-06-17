7c0e076dce047ece09d3f5191e05a9d5ca29751f
VolokhovOleg <volokhov.com@gmail.com>
2018-06-18 00:32:38 +0300

    Правки#3

--- js/map.js
+++ js/map.js
@@ -57,6 +57,8 @@ var template = document.querySelector('#ad-template').content.querySelector('.ma
 var mapPinsBlock = document.querySelector('.map__pins');
 var mapPin = document.querySelector('#ad-template').content.querySelector('.map__pin');
 
+var container = document.querySelector('.map__filters-container');
+
 var getRandomInteger = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -126,46 +128,37 @@ var renderPin = function (array) {
   var pinElement = mapPin.cloneNode(true);
   var pinImg = pinElement.querySelector('img');
 
-  var topPosition = 'top:' + (array.location.y - pinSize.HEIGHT / 2) + 'px;';
-  var leftPosition = 'left:' + (array.location.x - pinSize.WIDTH) + 'px;';
-  pinElement.style = topPosition + leftPosition;
+  var style = {
+    top: 'top:' + (array.location.y - pinSize.HEIGHT / 2) + 'px;',
+    left: 'left:' + (array.location.x - pinSize.WIDTH) + 'px;'
+  };
+
+  pinElement.style = style.top + style.left;
   pinImg.src = array.author.avatar;
   pinImg.alt = array.offer.title;
 
   mapPinsBlock.appendChild(pinElement);
 };
 
-var getGeneratedFeatures = function (adElement, card) {
-  for (var i = 0; i < card.length; i++) {
-    var featureElement = template.querySelector('.popup__feature--' + card[i]);
-    var element = featureElement.cloneNode();
-    adElement.querySelector('.popup__features').appendChild(element);
+var createElement = function (tagName, classes) {
+  tagName = document.createElement(tagName);
+  for (var i = 0; i < classes.length; i++) {
+    tagName.classList.add(classes[i]);
   }
+  return tagName;
 };
 
-var getGeneratedPhotoes = function (adElement, photoesArray) {
-  for (var i = 0; i < photoesArray.length; i++) {
-    var photoElement = template.querySelector('.popup__photo');
-    var element = photoElement.cloneNode();
-    element.src = photoesArray[i];
-    adElement.querySelector('.popup__photos').appendChild(element);
-  }
-};
+var getPopupPhoto = function (photo) {
+  photo.alt = 'Фотография жилья';
+  photo.width = 45;
+  photo.height = 45;
+  return photo;
+}
 
 var renderAd = function (card) {
   var adElement = template.cloneNode(true);
   var rooms = card.offer.rooms;
   var guests = card.offer.guests;
-
-  var featuresContent = adElement.querySelectorAll('.popup__feature');
-  for (var i = 0; i < featuresContent.length; i++) {
-    adElement.querySelector('.popup__features').removeChild(featuresContent[i]);
-  }
-
-  var photosContent = adElement.querySelectorAll('.popup__photo');
-  for (var j = 0; j < photosContent.length; j++) {
-    adElement.querySelector('.popup__photos').removeChild(photosContent[j]);
-  }
 
   adElement.querySelector('.popup__avatar').src = card.author.avatar;
   adElement.querySelector('.popup__title').textContent = card.offer.title;
@@ -174,9 +167,17 @@ var renderAd = function (card) {
   adElement.querySelector('.popup__type').textContent = offerTypesTranslation[card.offer.type];
   adElement.querySelector('.popup__text--capacity').textContent = rooms + ' ' + getPlurals(rooms, ['комната', 'комнаты', 'комнат']) + ' для ' + guests + ' ' + getPlurals(guests, ['гостя', 'гостей', 'гостей']);
   adElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
-  getGeneratedFeatures(adElement, card.offer.features);
+
+  for (var featureIndex = 0; featureIndex < card.offer.features.length; featureIndex++) {
+    adElement.querySelector('.popup__features').appendChild(createElement('li', ['popup__feature', 'popup__feature--' + card.offer.features[featureIndex]]));
+  }
+
   adElement.querySelector('.popup__description').textContent = card.offer.description;
-  getGeneratedPhotoes(adElement, card.offer.photos);
+
+  for (var photoIndex = 0; photoIndex < card.offer.photos.length; photoIndex++) {
+    var photo = getPopupPhoto(createElement('img', ['popup__photo']));
+    adElement.querySelector('.popup__photos').appendChild(photo).src = card.offer.photos[photoIndex];
+  }
 
   return adElement;
 };
@@ -195,7 +196,6 @@ var getAdsArray = function (amount) {
 };
 
 var generateAd = function (amount) {
-  var container = document.querySelector('.map__filters-container');
   var fragment = getAdsArray(amount);
 
   map.insertBefore(fragment, container);
